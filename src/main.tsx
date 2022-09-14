b1036bc425233aae191749296703be775b009aff
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-14 12:18:35 -0300

    vite with scss

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './screen/home'
+import './styles/styles.scss'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+)
