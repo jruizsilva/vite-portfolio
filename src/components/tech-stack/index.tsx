ba414b37966ac36271c8f257d0d54d7b4ad369a8
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-19 18:04:19 -0300

    modal added

--- src/components/tech-stack/index.tsx
+++ src/components/tech-stack/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import useHorizontalDragScroll from '../../hooks/useHorizontalDragScroll'
 import { Technology } from '../../types'
 
@@ -8,14 +9,50 @@ interface Props {
 const TechStack = ({ techList }: Props): JSX.Element => {
   const ulRef = useHorizontalDragScroll()
 
+  const [isOpenModal, setIsOpenModal] = useState(false)
+  const [techName, setTechName] = useState('')
+
+  const handleOpenModal = (name: string): void => {
+    console.log('open modal')
+    setIsOpenModal(true)
+    setTechName(name)
+  }
+
+  const handleCloseModal = (): void => {
+    console.log('close modal')
+    setIsOpenModal(false)
+    setTechName('')
+  }
+  console.log(isOpenModal)
+
   return (
-    <ul ref={ulRef} className='project__ul--tech-stack'>
-      {techList.map(({ id, svg, name }: Technology) => (
-        <li className='project__li--tech-stack' key={id}>
-          <img src={svg} alt={name} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul ref={ulRef} className='project__ul--tech-stack'>
+        {techList.map(({ id, svg, name }: Technology) => (
+          <li
+            key={id}
+            className='project__li--tech-stack'
+            onClick={() => handleOpenModal(name)}
+            data-title={name}
+          >
+            <img src={svg} alt={name} />
+          </li>
+        ))}
+      </ul>
+      {isOpenModal && (
+        <div className='project__overlay'>
+          <div className='project__modal'>
+            <h3 className='project__modal-title'>{techName}</h3>
+            <button
+              className='project__btn project__btn-modal-close'
+              onClick={handleCloseModal}
+            >
+              Close Me!
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
--- src/hooks/useHorizontalDragScroll.ts
+++ src/hooks/useHorizontalDragScroll.ts
@@ -2,20 +2,47 @@ import { useRef, useEffect } from 'react'
 
 const useHorizontalDragScroll = (): React.RefObject<HTMLUListElement> => {
   const ulRef = useRef<HTMLUListElement>(null)
-  // let isDown = false
-  // let startX
-  // let scrollLeft
+  let isDown = false
+  let startX: number
+  let scrollLeft: number
 
   useEffect(() => {
-    if (ulRef.current !== null) {
-      console.log(ulRef)
-      ulRef.current.addEventListener('mousedown', e => {})
-      ulRef.current.addEventListener('mouseleave', e => {})
-      ulRef.current.addEventListener('mouseup', e => {})
-      ulRef.current.addEventListener('mousemove', e => {})
+    const onClickPress = (e: MouseEvent): void => {
+      isDown = true
+      if (ulRef.current !== null) {
+        ulRef.current.classList.add('active')
+        startX = e.pageX - ulRef.current.offsetLeft
+        scrollLeft = ulRef.current.scrollLeft
+      }
+    }
+    const onMouseLeaveOrUp = (e: MouseEvent): void => {
+      isDown = false
+    }
+    const onMousePressAndMove = (e: MouseEvent): void => {
+      if (!isDown) return
+      e.preventDefault()
+      if (ulRef.current !== null) {
+        const x = e.pageX - ulRef.current.offsetLeft
+        const walk = x - startX
+        ulRef.current.scrollLeft = scrollLeft - walk
+      }
     }
 
-    return () => {}
+    if (ulRef.current !== null) {
+      ulRef.current.addEventListener('mousedown', onClickPress)
+      ulRef.current.addEventListener('mouseleave', onMouseLeaveOrUp)
+      ulRef.current.addEventListener('mouseup', onMouseLeaveOrUp)
+      ulRef.current.addEventListener('mousemove', onMousePressAndMove)
+    }
+
+    return () => {
+      if (ulRef.current !== null) {
+        ulRef.current.removeEventListener('mousedown', onClickPress)
+        ulRef.current.removeEventListener('mouseleave', onMouseLeaveOrUp)
+        ulRef.current.removeEventListener('mouseup', onMouseLeaveOrUp)
+        ulRef.current.removeEventListener('mousemove', onMousePressAndMove)
+      }
+    }
   }, [])
   return ulRef
 }
