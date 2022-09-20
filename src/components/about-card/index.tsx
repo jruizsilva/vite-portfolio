669fbad5d14b6103553763d136f8c29f5fc71750
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-19 22:48:04 -0300

    tablet design done

--- src/components/about-card/index.tsx
+++ src/components/about-card/index.tsx
@@ -2,16 +2,16 @@ import { icons } from '../../assets'
 
 const AboutCard = (): JSX.Element => {
   return (
-    <div className='about__card'>
-      <h3 className='about__h3'>Objetivo profesional</h3>
-      <p className='about__p'>
+    <div className='about-card'>
+      <h3 className='about-card__h3'>Objetivo profesional</h3>
+      <p className='about-card__p'>
         Soy un Full-stack developer Javascript con 1 año de experiencia
         académica en el desarrollo de software. Busco insertarme en la industria
         IT y desarrollar mis habilidades en un ámbito profesional. Mi distingo
         por mi colaboración en trabajo en equipo y adaptabilidad a los cambios.
       </p>
-      <ul className='about__ul'>
-        <li className='about__li'>
+      <ul className='about-card__ul'>
+        <li className='about-card__li'>
           <a
             href='https://github.com/jruizsilva'
             target='_blank'
@@ -20,7 +20,7 @@ const AboutCard = (): JSX.Element => {
             <img src={icons.linkedin} alt='linkedin' title='Linkedin profile' />
           </a>
         </li>
-        <li className='about__li'>
+        <li className='about-card__li'>
           <a
             href='https://www.linkedin.com/in/jruizsilva'
             target='_blank'
