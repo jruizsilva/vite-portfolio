23136729a9b4d982e6810caf338b54a16704a7aa
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-21 15:13:23 -0300

    header minor changes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,19 +7,21 @@ import { icons } from '../../assets'
 const Header = (): JSX.Element => {
   return (
     <header className='header'>
-      <div className='header__wrapper'>
-        <nav className='header__nav'>
-          <AnchorButton
-            href='http://google.com'
-            icon={icons.link}
-            bg='transparent'
-            border='#fff'
-          >
-            Descargar CV
-          </AnchorButton>
-          <ul className='header__ul'>
+      <nav className='header__nav'>
+        <ul className='header__ul'>
+          <li className='header__li'>
+            <AnchorButton
+              href='http://google.com'
+              icon={icons.link}
+              bg='transparent'
+              border='#fff'
+            >
+              Descargar CV
+            </AnchorButton>
+          </li>
+          <ul className='header__ul--links'>
             {navbarLinkList.map(({ id, href, name, offset }: NavbarLink) => (
-              <li key={id} className='header__li'>
+              <li key={id} className='header__li--links'>
                 <Link
                   className='header__a'
                   activeClass='header__a--active'
@@ -32,20 +34,23 @@ const Header = (): JSX.Element => {
                 </Link>
               </li>
             ))}
-            <li className='header__li'>
-              <select className='header__select'>
-                <option className='header__option' value='es'>
-                  ES
-                </option>
-                <option className='header__option' value='en'>
-                  EN
-                </option>
-              </select>
-            </li>
           </ul>
+
+          <li className='header__li'>
+            <select className='header__select'>
+              <option className='header__option' value='es'>
+                ES
+              </option>
+              <option className='header__option' value='en'>
+                EN
+              </option>
+            </select>
+          </li>
+        </ul>
+        <div className='header__burger'>
           <img className='header__img' src={icons.menu} alt='navbar menu' />
-        </nav>
-      </div>
+        </div>
+      </nav>
     </header>
   )
 }
