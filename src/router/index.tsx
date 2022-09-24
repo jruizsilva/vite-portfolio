59060f15f5fb466d2f5b2e1bce50eedf07cf8bcc
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-24 19:25:28 -0300

    animation on first load

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -10,29 +10,50 @@ import ScrollToTop from '../components/scroll-to-top/scroll-to-top'
 import HomeScreen from '../screen/home'
 import ProjectScreen from '../screen/project'
 import ProjectsScreen from '../screen/projects'
+import { useAppSelector } from '../hooks/useAppSelector'
+import { useAppDispatch } from '../hooks/useAppDispatch'
+import { useEffect } from 'react'
+import { setLoading } from '../redux/slice/ui'
 
 const Router = (): JSX.Element => {
   const location = useLocation()
+  const dispatch = useAppDispatch()
+  const { loading } = useAppSelector(state => state.ui)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      dispatch(setLoading(false))
+    }, 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
 
   return (
-    <div className='container'>
-      <ScrollToTop />
-      <AnimatePresence exitBeforeEnter>
-        <Routes location={location} key={location.key}>
-          <Route path='/' element={<HomeScreen />} />
-          <Route
-            path='/projects'
-            element={<ProjectsScreen />}
-          />
-          <Route
-            path='/project/:id'
-            element={<ProjectScreen />}
-          />
-          <Route path='/loader' element={<Loader />} />
-        </Routes>
-      </AnimatePresence>
-      <Footer />
-    </div>
+    <AnimatePresence exitBeforeEnter initial={false}>
+      {loading ? (
+        <Loader key={location.key} />
+      ) : (
+        <div className='container'>
+          <ScrollToTop />
+          <AnimatePresence exitBeforeEnter>
+            <Routes location={location} key={location.key}>
+              <Route path='/' element={<HomeScreen />} />
+              <Route
+                path='/projects'
+                element={<ProjectsScreen />}
+              />
+              <Route
+                path='/project/:id'
+                element={<ProjectScreen />}
+              />
+              <Route path='/loader' element={<Loader />} />
+            </Routes>
+          </AnimatePresence>
+          <Footer />
+        </div>
+      )}
+    </AnimatePresence>
   )
 }
 
--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -1,16 +1,10 @@
-import { useEffect } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 import About from '../../components/about'
 import Projects from '../../components/projects'
 import VerMas from '../../components/ver-mas'
 import Contact from '../../components/contact'
 import HeaderHome from '../../components/header-home'
-import { useAppSelector } from '../../hooks/useAppSelector'
-import { useAppDispatch } from '../../hooks/useAppDispatch'
-import { setLoading } from '../../redux/slice/ui'
-import Loader from '../../components/loader'
-import { useLocation } from 'react-router-dom'
 
 const containerVariants = {
   initial: { opacity: 0 },
@@ -19,45 +13,26 @@ const containerVariants = {
 }
 
 const HomeScreen = (): JSX.Element => {
-  const location = useLocation()
-  const { loading } = useAppSelector(state => state.ui)
-  const dispatch = useAppDispatch()
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      dispatch(setLoading(false))
-    }, 500)
-
-    return () => {
-      clearTimeout(timer)
-    }
-  }, [])
-
   return (
-    <AnimatePresence exitBeforeEnter>
-      {loading ? (
-        <Loader key={location.key} />
-      ) : (
-        <motion.div
-          variants={containerVariants}
-          initial='initial'
-          animate='animate'
-          exit='exit'
-        >
-          <HeaderHome />
-          <div className='wrapper'>
-            <About />
-            <Projects
-              title='Proyectos'
-              limit={2}
-              category='full-stack'
-            />
-            <VerMas />
-            <Contact />
-          </div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+    <motion.div
+      className='home'
+      variants={containerVariants}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+    >
+      <HeaderHome />
+      <div className='wrapper'>
+        <About />
+        <Projects
+          title='Proyectos'
+          limit={2}
+          category='full-stack'
+        />
+        <VerMas />
+        <Contact />
+      </div>
+    </motion.div>
   )
 }
 
