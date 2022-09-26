662bf26c9448d4b2458dac8a97ce8bb114421c8b
jruizsilva <ruizsilvajonathan@gmail.com>
2022-09-26 04:15:09 -0300

    projects animation on view

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useRef } from 'react'
+import {
+  motion,
+  AnimatePresence,
+  useInView
+} from 'framer-motion'
 import ProjectCard from '../project-card'
 import { projectList } from './const'
 import SectionTitle from '../section-title'
+
+const titleVariants = {
+  initial: { opacity: 0, y: -50 },
+  animate: (i: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: i * 0.3
+    }
+  })
+}
 
 interface Props {
   title?: string
@@ -13,20 +30,51 @@ const Projects = ({
   limit = projectList.length,
   category
 }: Props): JSX.Element => {
+  const ref = useRef(null)
+  const isInView = useInView(ref, {
+    margin: '0px 0px -350px 0px'
+  })
+
+  useEffect(() => {
+    console.log('Element Projects: ', isInView)
+  }, [isInView])
+
   return (
-    <div className='projects' id='projects'>
-      {title !== undefined && (
-        <SectionTitle>{title}</SectionTitle>
-      )}
-      <ul className='projects__ul'>
-        {projectList.length > 0 &&
-          projectList
-            .filter(p => p.category === category)
-            .slice(0, limit)
-            .map(project => (
-              <ProjectCard key={project.id} {...project} />
-            ))}
-      </ul>
+    <div ref={ref} className='projects' id='projects'>
+      <AnimatePresence exitBeforeEnter>
+        {title !== undefined && isInView && (
+          <SectionTitle
+            variants={titleVariants}
+            initial='initial'
+            animate='animate'
+            custom={1}
+          >
+            {title}
+          </SectionTitle>
+        )}
+      </AnimatePresence>
+      <AnimatePresence exitBeforeEnter>
+        {isInView && (
+          <motion.ul
+            className='projects__ul'
+            variants={titleVariants}
+            initial='initial'
+            animate='animate'
+            custom={2}
+          >
+            {projectList.length > 0 &&
+              projectList
+                .filter(p => p.category === category)
+                .slice(0, limit)
+                .map(project => (
+                  <ProjectCard
+                    key={project.id}
+                    {...project}
+                  />
+                ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
--- src/components/section-title/index.tsx
+++ src/components/section-title/index.tsx
@@ -1,8 +1,29 @@
-import { useInView, motion, Variants } from 'framer-motion'
-import { useEffect, useRef } from 'react'
+import { motion, Variants } from 'framer-motion'
 import styled from 'styled-components'
 
 const StyledTitle = styled(motion.h2)`
+  text-align: center;
+  font-size: 24px;
+  font-weight: 500;
+  padding: 16px 0;
+
+  @media (min-width: 425px) {
+    font-size: 32px;
+    padding: 24px 0;
+  }
+  @media (min-width: 768px) {
+    font-size: 32px;
+    padding: 32px;
+  }
+`
+
+interface PropsTitleShowInView {
+  isInView: boolean
+}
+
+const StyledTitleShowInView = styled(
+  motion.h2
+)<PropsTitleShowInView>`
   text-align: center;
   font-size: 24px;
   font-weight: 500;
@@ -24,24 +45,32 @@ interface Props {
   initial?: string
   animate?: string
   custom?: number
+  isInView?: boolean
 }
 
 const SectionTitle = ({
   children,
+  isInView,
   ...props
 }: Props): JSX.Element => {
-  const ref = useRef(null)
-  const isInView = useInView(ref, {
-    margin: '0px 0px -300px 0px'
-  })
-
-  useEffect(() => {
-    console.log(isInView)
-  }, [isInView])
   return (
-    <StyledTitle ref={ref} {...props}>
-      {children}
-    </StyledTitle>
+    <>
+      {isInView === true ? (
+        <StyledTitleShowInView
+          initial={{ opacity: 0 }}
+          whileInView={{
+            opacity: 1,
+            transition: { duration: 1 }
+          }}
+          isInView={isInView}
+          {...props}
+        >
+          {children}
+        </StyledTitleShowInView>
+      ) : (
+        <StyledTitle {...props}>{children}</StyledTitle>
+      )}
+    </>
   )
 }
 
