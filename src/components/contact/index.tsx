1139736c0a0a8a39885565d76622727cd8847ac1
jruizsilva <ruizsilvajonathan@gmail.com>
2022-10-05 18:21:22 -0300

    contact form done

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -8,6 +8,8 @@ import { FormField } from '../../types'
 import { Button } from '../buttons'
 import SectionTitle from '../section-title'
 import { contactFields } from './const'
+import emailjs from '@emailjs/browser'
+import toast, { Toaster } from 'react-hot-toast'
 
 const contactVariants: Variants = {
   initial: { opacity: 0, x: -50, scale: 0.2 },
@@ -28,156 +30,176 @@ const initialValues = {
 }
 
 const Contact = (): JSX.Element => {
-  const { t, i18n } = useTranslation('contact')
+  const { t } = useTranslation('contact')
   const form = useRef<HTMLFormElement>(null)
 
+  const sendEmail = (form: HTMLFormElement): void => {
+    toast
+      .promise(
+        emailjs.sendForm(
+          'service_7bw4o4q',
+          'template_2spbbys',
+          form,
+          'omBJgOPXFkjqPOqNn'
+        ),
+        {
+          loading: t('contact.toast.loading'),
+          success: t('contact.toast.success'),
+          error: t('contact.toast.error')
+        }
+      )
+      .then(() => {
+        console.log('Email sended')
+      })
+      .catch(console.log)
+  }
+
   return (
-    <motion.div
-      className='contact'
-      id='contact'
-      initial='initial'
-      whileInView='animate'
-      viewport={{
-        once: true,
-        margin: '0px 0px -300px 0px'
-      }}
-      transition={{ staggerChildren: 0.3 }}
-    >
-      <SectionTitle>{t('contact.title')}</SectionTitle>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={yup.object({
-          email: yup
-            .string()
-            .min(6, 'contact.validation.email.minlength')
-            .email('contact.validation.email.valid')
-            .max(24, 'contact.validation.email.maxlength')
-            .required('contact.validation.email.required'),
-          subject: yup
-            .string()
-            .min(4, 'contact.validation.subject.minlength')
-            .max(
-              128,
-              'contact.validation.subject.maxlength'
-            )
-            .required(
-              'contact.validation.subject.required'
-            ),
-          message: yup
-            .string()
-            .min(
-              4,
-              'contact.validation.description.minlength'
-            )
-            .max(
-              256,
-              'contact.validation.description.maxlength'
-            )
-            .required(
-              'contact.validation.description.required'
-            )
-        })}
-        onSubmit={values => {
-          console.log(values)
+    <>
+      <motion.div
+        className='contact'
+        id='contact'
+        initial='initial'
+        whileInView='animate'
+        viewport={{
+          once: true,
+          margin: '0px 0px -300px 0px'
         }}
+        transition={{ staggerChildren: 0.3 }}
       >
-        {props => {
-          const {
-            values,
-            touched,
-            errors,
-            isSubmitting,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            setFieldTouched
-          } = props
-          i18n.on('languageChanged', () => {
-            Object.keys(errors).forEach(fieldName => {
-              if (
-                Object.keys(touched).includes(fieldName)
-              ) {
-                setFieldTouched(fieldName)
-              }
-            })
-          })
-          return (
-            <motion.form
-              ref={form}
-              onSubmit={handleSubmit}
-              className='contact__form'
-              variants={contactVariants}
-            >
-              {contactFields.map(
-                ({ id, field, type }: FormField) => (
-                  <div key={id} className='contact__box'>
-                    <label className='contact__label'>
-                      {t(`contact.form.${field}.label`)} 👇
-                    </label>
-                    {type === 'textarea' ? (
-                      <textarea
-                        className={`contact__textarea ${
-                          errors[field] !== undefined &&
-                          touched[field] !== undefined
-                            ? 'error'
-                            : ''
-                        }`}
-                        placeholder={t(
-                          `contact.form.${field}.placeholder`
-                        )}
-                        name={field}
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values[field]}
-                        autoComplete='off'
-                      ></textarea>
-                    ) : (
-                      <input
-                        className={`contact__input ${
-                          errors[field] !== undefined &&
-                          touched[field] !== undefined
-                            ? 'error'
-                            : ''
-                        }`}
-                        placeholder={t(
-                          `contact.form.${field}.placeholder`
-                        )}
-                        name={field}
-                        onBlur={handleBlur}
-                        onChange={handleChange}
-                        value={values[field]}
-                        autoComplete='off'
-                      />
-                    )}
-                    {errors[field] !== undefined &&
-                      touched[field] !== undefined && (
-                        <span className='contact__error-message'>
-                          {t(errors[field] as string)}
-                        </span>
+        <SectionTitle>{t('contact.title')}</SectionTitle>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={yup.object({
+            email: yup
+              .string()
+              .min(6, 'contact.validation.email.minlength')
+              .email('contact.validation.email.valid')
+              .max(24, 'contact.validation.email.maxlength')
+              .required(
+                'contact.validation.email.required'
+              ),
+            subject: yup
+              .string()
+              .min(
+                4,
+                'contact.validation.subject.minlength'
+              )
+              .max(
+                128,
+                'contact.validation.subject.maxlength'
+              )
+              .required(
+                'contact.validation.subject.required'
+              ),
+            message: yup
+              .string()
+              .min(
+                4,
+                'contact.validation.description.minlength'
+              )
+              .max(
+                256,
+                'contact.validation.description.maxlength'
+              )
+              .required(
+                'contact.validation.description.required'
+              )
+          })}
+          onSubmit={() =>
+            sendEmail(form.current as HTMLFormElement)
+          }
+        >
+          {props => {
+            const {
+              values,
+              touched,
+              errors,
+              isSubmitting,
+              handleChange,
+              handleBlur,
+              handleSubmit
+            } = props
+            return (
+              <motion.form
+                ref={form}
+                onSubmit={handleSubmit}
+                className='contact__form'
+                variants={contactVariants}
+              >
+                {contactFields.map(
+                  ({ id, field, type }: FormField) => (
+                    <div key={id} className='contact__box'>
+                      <label className='contact__label'>
+                        {t(`contact.form.${field}.label`)}{' '}
+                        👇
+                      </label>
+                      {type === 'textarea' ? (
+                        <textarea
+                          className={`contact__textarea ${
+                            errors[field] !== undefined &&
+                            touched[field] !== undefined
+                              ? 'error'
+                              : ''
+                          }`}
+                          placeholder={t(
+                            `contact.form.${field}.placeholder`
+                          )}
+                          name={field}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values[field]}
+                          autoComplete='off'
+                        ></textarea>
+                      ) : (
+                        <input
+                          className={`contact__input ${
+                            errors[field] !== undefined &&
+                            touched[field] !== undefined
+                              ? 'error'
+                              : ''
+                          }`}
+                          placeholder={t(
+                            `contact.form.${field}.placeholder`
+                          )}
+                          name={field}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          value={values[field]}
+                          autoComplete='off'
+                        />
                       )}
-                  </div>
-                )
-              )}
-              <div className='contact__box'>
-                <Button
-                  type='submit'
-                  icon={icons.send}
-                  w='100%'
-                  bg='transparent'
-                  border='#fff'
-                  disabled={
-                    isSubmitting ||
-                    Object.keys(errors).length > 0
-                  }
-                >
-                  {t('contact.button.submit')}
-                </Button>
-              </div>
-            </motion.form>
-          )
-        }}
-      </Formik>
-    </motion.div>
+                      {errors[field] !== undefined &&
+                        touched[field] !== undefined && (
+                          <span className='contact__error-message'>
+                            {t(errors[field] as string)}
+                          </span>
+                        )}
+                    </div>
+                  )
+                )}
+                <div className='contact__box'>
+                  <Button
+                    type='submit'
+                    icon={icons.send}
+                    w='100%'
+                    bg='transparent'
+                    border='#fff'
+                    disabled={
+                      isSubmitting ||
+                      Object.keys(errors).length > 0
+                    }
+                  >
+                    {t('contact.button.submit')}
+                  </Button>
+                </div>
+              </motion.form>
+            )
+          }}
+        </Formik>
+      </motion.div>
+      <Toaster position='bottom-right' />
+    </>
   )
 }
 
