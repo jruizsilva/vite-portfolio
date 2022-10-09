525a66e689c82d8195343730ee53c2d1b8c395eb
jruizsilva <ruizsilvajonathan@gmail.com>
2022-10-09 01:45:09 -0300

    images added

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,3 +1,4 @@
+// ICONS ↓↓
 import link from './svg/link.svg'
 import menu from './svg/menu.svg'
 import linkedin from './svg/linkedin.svg'
@@ -16,21 +17,51 @@ import sequelize from './svg/sequelize.svg'
 import close from './svg/close.svg'
 import close_white from './svg/close_white.svg'
 // HTML-CSS ↓↓
-import notfound from './html-css/not-found.png'
-import portfolio2021 from './html-css/portfolio.png'
-import checkout from './html-css/checkout-page.png'
+import checkout from './html-css/checkout.png'
+import documentation from './html-css/documentation.png'
 import ediehomepage from './html-css/edie-homepage.png'
+import gallery from './html-css/gallery.png'
 import interiorconsultant from './html-css/interior-consultant.png'
-import mygallery from './html-css/my-gallery.png'
-import recipe from './html-css/recipe-page.png'
-import team from './html-css/team-page.png'
+import landingpage from './html-css/landing-page.png'
+import notfound from './html-css/not-found.png'
+import billieporfolio from './html-css/billie-portfolio.png'
+import recipe from './html-css/recipe.png'
+import surveyform from './html-css/survey-form.png'
+import team from './html-css/team.png'
+import travelsblog from './html-css/travels-blog.png'
+import tribute from './html-css/tribute.png'
+import universidad from './html-css/universidad.png'
+import portfolio2021 from './html-css/portfolio2021.png'
 // FRONT-END ↓↓
-import button from './front-end/buttons.png'
-import input from './front-end/input-component.png'
+import breakingbad from './front-end/breaking-bad.png'
+import button from './front-end/button.png'
+import clientadmin from './front-end/client-admin.png'
+import crudindexeddb from './front-end/crud-indexeddb.png'
+import dogs from './front-end/dogs.png'
+import domexercices from './front-end/dom-exercices.png'
+import formvalidation from './front-end/form-validation.png'
+import foxnine from './front-end/foxnine.png'
+import input from './front-end/input.png'
+import localstorage from './front-end/local-storage.png'
+import movielist from './front-end/movie-list.png'
+import myanimelist from './front-end/my-list-anime.png'
+import pokelist from './front-end/poke-list.png'
+import portfolio2022 from './front-end/portfolio2022.png'
+import quoteinsurance from './front-end/quote-insurance.png'
+import randomquotemachine from './front-end/random-quote-machine.png'
+import carsearcher from './front-end/car-searcher.png'
+import songsearcher from './front-end/song-searcher.png'
+import todoapp from './front-end/todo-app.png'
+import todoapp2 from './front-end/todo-app2.png'
+import veterinary from './front-end/veterinary.png'
+import weather from './front-end/weather.png'
+import weeklyexpense from './front-end/weekly-expense.png'
 // FULL-STACK ↓↓
-import marketplace from './full-stack/marketplace-app.png'
-import myfinances from './full-stack/my-finances-app.png'
-import uploadyourimages from './full-stack/upload-your-images.png'
+import chat from './full-stack/chat.png'
+import countriespi from './full-stack/countries-pi.png'
+import finances from './full-stack/finances.png'
+import marketplace from './full-stack/marketplace-pf.png'
+import uploadimages from './full-stack/upload-images.png'
 
 const icons = {
   link,
@@ -54,23 +85,53 @@ const icons = {
 
 const projects = {
   'html-css': {
-    notfound,
-    portfolio2021,
     checkout,
+    documentation,
     ediehomepage,
+    gallery,
     interiorconsultant,
-    mygallery,
+    landingpage,
+    notfound,
+    billieporfolio,
     recipe,
-    team
+    surveyform,
+    team,
+    tribute,
+    universidad,
+    travelsblog,
+    portfolio2021
   },
   'front-end': {
+    breakingbad,
     button,
-    input
+    clientadmin,
+    crudindexeddb,
+    dogs,
+    domexercices,
+    formvalidation,
+    foxnine,
+    input,
+    localstorage,
+    movielist,
+    myanimelist,
+    pokelist,
+    portfolio2022,
+    quoteinsurance,
+    randomquotemachine,
+    carsearcher,
+    songsearcher,
+    todoapp,
+    todoapp2,
+    veterinary,
+    weather,
+    weeklyexpense
   },
   'full-stack': {
     marketplace,
-    myfinances,
-    uploadyourimages
+    finances,
+    uploadimages,
+    chat,
+    countriespi
   }
 }
 
--- src/components/projects/const.ts
+++ src/components/projects/const.ts
@@ -13,108 +13,8 @@ const htmlcssList: projectListType = [
   {
     id: 1,
     github:
-      'https://github.com/jruizsilva/404-not-found-devchallengs.io',
-    deploy: 'https://devchallenges-project-1.netlify.app',
-    techList: [
-      {
-        id: 1,
-        title: 'html',
-        svg: icons.html
-      },
-      {
-        id: 2,
-        title: 'css',
-        svg: icons.css3
-      }
-    ],
-    overview: projects['html-css'].notfound,
-    category: 'html-css'
-  },
-  {
-    id: 2,
-    github:
-      'https://github.com/jruizsilva/my-team-page-devchallenges.io',
-    deploy: 'https://devchallenges-project-2.netlify.app/',
-    techList: [
-      {
-        id: 1,
-        title: 'html',
-        svg: icons.html
-      },
-      {
-        id: 2,
-        title: 'css',
-        svg: icons.css3
-      }
-    ],
-    overview: projects['html-css'].team,
-    category: 'html-css'
-  },
-  {
-    id: 3,
-    github:
-      'https://github.com/jruizsilva/interior-consultant-devchallenges.io',
-    deploy: 'https://devchallenges-project-3.netlify.app/',
-    techList: [
-      {
-        id: 1,
-        title: 'html',
-        svg: icons.html
-      },
-      {
-        id: 2,
-        title: 'css',
-        svg: icons.css3
-      }
-    ],
-    overview: projects['html-css'].interiorconsultant,
-    category: 'html-css'
-  },
-  {
-    id: 4,
-    github:
-      'https://github.com/jruizsilva/recipe-page-devchallenges.io',
-    deploy: 'https://devchallenges-project-4.netlify.app/',
-    techList: [
-      {
-        id: 1,
-        title: 'html',
-        svg: icons.html
-      },
-      {
-        id: 2,
-        title: 'css',
-        svg: icons.css3
-      }
-    ],
-    overview: projects['html-css'].recipe,
-    category: 'html-css'
-  },
-  {
-    id: 5,
-    github:
-      'https://github.com/jruizsilva/my-gallery-devchallenges.io',
-    deploy: 'https://devchallenges-project-5.netlify.app/',
-    techList: [
-      {
-        id: 1,
-        title: 'html',
-        svg: icons.html
-      },
-      {
-        id: 2,
-        title: 'css',
-        svg: icons.css3
-      }
-    ],
-    overview: projects['html-css'].mygallery,
-    category: 'html-css'
-  },
-  {
-    id: 6,
-    github:
       'https://github.com/jruizsilva/checkout-page-devchallenges.io',
-    deploy: 'https://devchellenges-project-6.netlify.app/',
+    deploy: 'https://devchallenges-project-6.netlify.app',
     techList: [
       {
         id: 1,
@@ -131,7 +31,28 @@ const htmlcssList: projectListType = [
     category: 'html-css'
   },
   {
-    id: 7,
+    id: 2,
+    github:
+      'https://github.com/jruizsilva/documentation-page',
+    deploy:
+      'https://jruizsilva-documentation-page.netlify.app',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].documentation,
+    category: 'html-css'
+  },
+  {
+    id: 3,
     github:
       'https://github.com/jruizsilva/edie-homepage-devchallenges.io',
     deploy: 'https://devchallenges-project-7.netlify.app/',
@@ -151,10 +72,226 @@ const htmlcssList: projectListType = [
     category: 'html-css'
   },
   {
+    id: 4,
+    github:
+      'https://github.com/jruizsilva/my-gallery-devchallenges.io',
+    deploy: 'https://devchallenges-project-5.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].gallery,
+    category: 'html-css'
+  },
+  {
+    id: 5,
+    github:
+      'https://github.com/jruizsilva/interior-consultant-devchallenges.io',
+    deploy: 'https://devchallenges-project-3.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].interiorconsultant,
+    category: 'html-css'
+  },
+  {
+    id: 6,
+    github: 'https://github.com/jruizsilva/landing-page',
+    deploy: 'https://jruizsilva-landing-page.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].landingpage,
+    category: 'html-css'
+  },
+  {
+    id: 7,
+    github:
+      'https://github.com/jruizsilva/404-not-found-devchallengs.io',
+    deploy: 'https://devchallenges-project-1.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].notfound,
+    category: 'html-css'
+  },
+  {
     id: 8,
     github:
-      'https://github.com/jruizsilva/portfolio-devchallenges.io',
-    deploy: 'https://devchallenges-project-8.netlify.app/',
+      'https://github.com/jruizsilva/personal-portfolio-webpage',
+    deploy: 'https://jruizsilva.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].billieporfolio,
+    category: 'html-css'
+  },
+  {
+    id: 9,
+    github:
+      'https://github.com/jruizsilva/recipe-page-devchallenges.io',
+    deploy: 'https://devchallenges-project-4.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].recipe,
+    category: 'html-css'
+  },
+  {
+    id: 10,
+    github: 'https://github.com/jruizsilva/survey-form',
+    deploy: 'https://jruizsilva-survey-form.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].surveyform,
+    category: 'html-css'
+  },
+  {
+    id: 11,
+    github:
+      'https://github.com/jruizsilva/my-team-page-devchallenges.io',
+    deploy: 'https://devchallenges-project-2.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].team,
+    category: 'html-css'
+  },
+  {
+    id: 12,
+    github: 'https://github.com/jruizsilva/tribute-page',
+    deploy: 'https://jruizsilva-tribute-page.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].tribute,
+    category: 'html-css'
+  },
+  {
+    id: 13,
+    github: 'https://github.com/jruizsilva/carrito-cursos',
+    deploy: 'https://js-moderno-project-1.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].universidad,
+    category: 'html-css'
+  },
+  {
+    id: 14,
+    github:
+      'https://github.com/jruizsilva/blog-de-viajes-curso',
+    deploy: 'https://angry-easley-d5a70b.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['html-css'].travelsblog,
+    category: 'html-css'
+  },
+  {
+    id: 15,
+    github:
+      'https://github.com/jruizsilva/personal-portfolio-webpage',
+    deploy: 'https://jruizsilva.netlify.app/',
     techList: [
       {
         id: 1,
@@ -176,6 +313,26 @@ const frontendList: projectListType = [
   {
     id: 1,
     github:
+      'https://github.com/jruizsilva/breaking-bad-app',
+    deploy: 'https://curso-practico-react-2.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].breakingbad,
+    category: 'front-end'
+  },
+  {
+    id: 2,
+    github:
       'https://github.com/jruizsilva/button-component-devchallenges.io',
     deploy: 'https://devchallenges-react-1.netlify.app/',
     techList: [
@@ -194,7 +351,103 @@ const frontendList: projectListType = [
     category: 'front-end'
   },
   {
-    id: 2,
+    id: 3,
+    github: 'https://github.com/jruizsilva/cms-indexeddb',
+    deploy: 'https://js-moderno-project-8.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].clientadmin,
+    category: 'front-end'
+  },
+  {
+    id: 4,
+    github: 'https://github.com/jruizsilva/dog-app',
+    deploy: 'https://curso-practico-react-3.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].dogs,
+    category: 'front-end'
+  },
+  {
+    id: 5,
+    github: 'https://github.com/jruizsilva/ejercicios-dom',
+    deploy: 'https://curso-js-jonmircha-dom.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].domexercices,
+    category: 'front-end'
+  },
+  {
+    id: 6,
+    github: 'https://github.com/jruizsilva/validar-email',
+    deploy: 'https://js-moderno-project-2.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].formvalidation,
+    category: 'front-end'
+  },
+  {
+    id: 7,
+    github:
+      'https://github.com/jruizsilva/button-component-devchallenges.io',
+    deploy: 'https://devchallenges-react-1.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].foxnine,
+    category: 'front-end'
+  },
+  {
+    id: 8,
     github:
       'https://github.com/jruizsilva/input-component-devchallenges.io',
     deploy: 'https://dev-challenges-react-2.netlify.app/',
@@ -211,6 +464,280 @@ const frontendList: projectListType = [
       }
     ],
     overview: projects['front-end'].input,
+    category: 'front-end'
+  },
+  {
+    id: 9,
+    github:
+      'https://github.com/jruizsilva/lista-tareas-local-storage',
+    deploy: 'https://jnt-project-1.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].localstorage,
+    category: 'front-end'
+  },
+  {
+    id: 10,
+    github:
+      'https://github.com/jruizsilva/movie-list-with-use-context',
+    deploy:
+      'https://movie-list-with-use-context.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].movielist,
+    category: 'front-end'
+  },
+  {
+    id: 11,
+    github: 'https://github.com/jruizsilva/miListaAnime',
+    deploy: 'https://jnt-project-3.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].myanimelist,
+    category: 'front-end'
+  },
+  {
+    id: 12,
+    github: 'https://github.com/jruizsilva/pokeList',
+    deploy: 'https://jruizsilva-pokelist.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].pokelist,
+    category: 'front-end'
+  },
+  {
+    id: 13,
+    github: 'https://github.com/jruizsilva/vite-portfolio',
+    deploy: 'https://vite-portfolio-jruizsilva.vercel.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].portfolio2022,
+    category: 'front-end'
+  },
+  {
+    id: 14,
+    github:
+      'https://github.com/jruizsilva/cotizador-seguro-prototype',
+    deploy: 'https://js-moderno-project-5.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].quoteinsurance,
+    category: 'front-end'
+  },
+  {
+    id: 15,
+    github:
+      'https://github.com/jruizsilva/random-quote-machine',
+    deploy:
+      'https://jruizsilva-random-quote-machine.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].randomquotemachine,
+    category: 'front-end'
+  },
+  {
+    id: 16,
+    github: 'https://github.com/jruizsilva/buscador-autos',
+    deploy: 'https://js-moderno-project-3.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].carsearcher,
+    category: 'front-end'
+  },
+  {
+    id: 17,
+    github: 'https://github.com/jruizsilva/lyrics',
+    deploy: 'https://js-moderno-project-10.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].songsearcher,
+    category: 'front-end'
+  },
+  {
+    id: 18,
+    github:
+      'https://github.com/jruizsilva/lista-tareas-local-storage',
+    deploy: 'https://jnt-project-1.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].todoapp,
+    category: 'front-end'
+  },
+  {
+    id: 19,
+    github: 'https://github.com/jruizsilva/react-todo-app',
+    deploy: 'https://curso-practico-react-1.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].todoapp2,
+    category: 'front-end'
+  },
+  {
+    id: 20,
+    github:
+      'https://github.com/jruizsilva/veterinaria-administrador',
+    deploy: 'https://js-moderno-project-7.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].veterinary,
+    category: 'front-end'
+  },
+  {
+    id: 21,
+    github: 'https://github.com/jruizsilva/clima',
+    deploy: 'https://js-moderno-project-9.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].weather,
+    category: 'front-end'
+  },
+  {
+    id: 22,
+    github: 'https://github.com/jruizsilva/gasto-semanal',
+    deploy: 'https://js-moderno-project-6.netlify.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      }
+    ],
+    overview: projects['front-end'].weeklyexpense,
     category: 'front-end'
   }
 ]
@@ -313,7 +840,7 @@ const fullstackList: projectListType = [
         svg: icons.sequelize
       }
     ],
-    overview: projects['full-stack'].myfinances,
+    overview: projects['full-stack'].finances,
     category: 'full-stack'
   },
   {
@@ -364,7 +891,106 @@ const fullstackList: projectListType = [
         svg: icons.sequelize
       }
     ],
-    overview: projects['full-stack'].uploadyourimages,
+    overview: projects['full-stack'].uploadimages,
+    category: 'full-stack'
+  },
+  {
+    id: 4,
+    github:
+      'https://github.com/jruizsilva/chat-group-devchallenges.io',
+    deploy: 'https://chat-group-devchallenges.vercel.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      },
+      {
+        id: 3,
+        title: 'javascript',
+        svg: icons.javascript
+      },
+      {
+        id: 4,
+        title: 'react',
+        svg: icons.react
+      },
+      {
+        id: 5,
+        title: 'redux',
+        svg: icons.redux
+      },
+      {
+        id: 6,
+        title: 'express',
+        svg: icons.express
+      },
+      {
+        id: 7,
+        title: 'postgre',
+        svg: icons.postgresql
+      },
+      {
+        id: 8,
+        title: 'sequelize',
+        svg: icons.sequelize
+      }
+    ],
+    overview: projects['full-stack'].chat,
+    category: 'full-stack'
+  },
+  {
+    id: 5,
+    github: 'https://github.com/jruizsilva/countries-pi',
+    deploy: 'https://countries-pi-jruizsilva.vercel.app/',
+    techList: [
+      {
+        id: 1,
+        title: 'html',
+        svg: icons.html
+      },
+      {
+        id: 2,
+        title: 'css',
+        svg: icons.css3
+      },
+      {
+        id: 3,
+        title: 'javascript',
+        svg: icons.javascript
+      },
+      {
+        id: 4,
+        title: 'react',
+        svg: icons.react
+      },
+      {
+        id: 5,
+        title: 'redux',
+        svg: icons.redux
+      },
+      {
+        id: 6,
+        title: 'express',
+        svg: icons.express
+      },
+      {
+        id: 7,
+        title: 'postgre',
+        svg: icons.postgresql
+      },
+      {
+        id: 8,
+        title: 'sequelize',
+        svg: icons.sequelize
+      }
+    ],
+    overview: projects['full-stack'].countriespi,
     category: 'full-stack'
   }
 ]
